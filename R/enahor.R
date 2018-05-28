e722f5d4fcf340a54ac9e106b2ca5469f0dd888b
Mesías Raúl Panduro Chávez <mesiaspch64@gmail.com>
2018-05-27 22:47:51 -0500

    Creando modulos en el paquete

---
+++ R/enahor.R
@@ -0,0 +1,18 @@
+#' Trabaja con mapas de Peru. Creado por BEST
+#'
+#' @name ENAHOR
+#' @docType package
+
+NULL
+
+#' Base de datos para trabajar con informacion socieconomica del Peru
+#'
+#' Base de datos para trabajar con informacion socieconomica del Peru
+#'
+#' @docType data
+#' @keywords datasets
+#' @name Modulo01
+#' @usage data(Modulo01)
+#' @format Base de datos con la informacion de las Caracteristicas de la Vivienda y del Hogar.
+#'
+NULL
