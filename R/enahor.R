7a58f571cce9ce7b0482152fd8a33aded642488c
Mesías Raúl Panduro Chávez <mesiaspch64@gmail.com>
2018-05-27 22:57:52 -0500

    Elaborando manuales
    Se borro hello

--- R/enahor.R
+++ R/enahor.R
@@ -16,3 +16,51 @@ NULL
 #' @format Base de datos con la informacion de las Caracteristicas de la Vivienda y del Hogar.
 #'
 NULL
+
+#' Base de datos para trabajar con informacion socieconomica del Peru
+#'
+#' Base de datos para trabajar con informacion socieconomica del Peru
+#'
+#' @docType data
+#' @keywords datasets
+#' @name Modulo02
+#' @usage data(Modulo02)
+#' @format Base de datos con la informacion de las Características de los Miembros del Hogar.
+#'
+NULL
+
+#' Base de datos para trabajar con informacion socieconomica del Peru
+#'
+#' Base de datos para trabajar con informacion socieconomica del Peru
+#'
+#' @docType data
+#' @keywords datasets
+#' @name Modulo03
+#' @usage data(Modulo03)
+#' @format Base de datos con la informacion de las Características de Educación.
+#'
+NULL
+
+#' Base de datos para trabajar con informacion socieconomica del Peru
+#'
+#' Base de datos para trabajar con informacion socieconomica del Peru
+#'
+#' @docType data
+#' @keywords datasets
+#' @name Modulo04
+#' @usage data(Modulo04)
+#' @format Base de datos con la informacion de Salud.
+#'
+NULL
+
+#' Base de datos para trabajar con informacion socieconomica del Peru
+#'
+#' Base de datos para trabajar con informacion socieconomica del Peru
+#'
+#' @docType data
+#' @keywords datasets
+#' @name Modulo05
+#' @usage data(Modulo05)
+#' @format Base de datos con la informacion de Empleo e Ingresos.
+#'
+NULL
